b91daf6bfe48960447bcfd849b7fceea7d4405cc
A.S.M. Hasan Sarker <asmhasansarker@gmail.com>
2022-07-03 18:33:36 +0600

    course section end

--- src/components/PopularCourseSlider/PopularCourseSlider.js
+++ src/components/PopularCourseSlider/PopularCourseSlider.js
@@ -4,6 +4,8 @@ import webDesignAndDevelopment from "../assets/webDesignAndDevelopment.jpg";
 import digitalMarketing from "../assets/digitalMarketing.jpg";
 import msOffice from "../assets/msOfficeMale.jpg";
 import kidsProgramming from "../assets/kidsProgramming.jpg";
+import graphicDesign from "../assets/graphicDesign.jpg";
+import hscICT from "../assets/hscICT.jpg";
 import CourseCard from "./courseCard";
 
 export default class PopularCourseSlider extends Component {
@@ -33,6 +35,18 @@ export default class PopularCourseSlider extends Component {
         <CourseCard
           image={kidsProgramming}
           title="Programming for KIDS"
+          description="With supporting text below as a natural lead-in to additional
+                content."
+        />
+        <CourseCard
+          image={graphicDesign}
+          title="Graphic Design"
+          description="With supporting text below as a natural lead-in to additional
+                content."
+        />
+        <CourseCard
+          image={hscICT}
+          title="HSC ICT"
           description="With supporting text below as a natural lead-in to additional
                 content."
         />
