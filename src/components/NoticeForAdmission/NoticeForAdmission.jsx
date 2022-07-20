f397bf6afce7863221cec5db60cb519e8e0b90af
asmhasansarker <asmhasansarker@gmail.com>
2022-07-20 20:45:09 +0600

    notice updated

--- src/components/NoticeForAdmission/NoticeForAdmission.jsx
+++ src/components/NoticeForAdmission/NoticeForAdmission.jsx
@@ -5,7 +5,7 @@ const NoticeForAdmission = () => {
   return (
       <Marquee speed={150}className='display-6 text-danger bg-info py-3 px-3' gradient={false} pauseOnHover={true}>
        
-          <h1 className='me-3'>জাতীয় যুব ও কারিগরি প্রশিক্ষণ কেন্দ্রে ভর্তি চলছে। এস.এস.সি পরিক্ষার্থীদের জন্য বিশেষ ছাড়।</h1>
+          <h1 className='me-3'>জাতীয় যুব ও কারিগরি প্রশিক্ষণ কেন্দ্রে ভর্তি চলছে। এস.এস.সি পরিক্ষার্থীদের জন্য ৫০%  ছাড়ে ভর্তির সুযোগ।</h1>
         
     </Marquee>
   )
